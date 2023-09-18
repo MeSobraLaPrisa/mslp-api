7d24ff8629190c248636bc06571ed5d99e6e06c7
gonzaloconcep <lasconchasgonzalo@gmail.com>
2023-09-18 23:51:09 +0200

    Fix: postgresql env variable

--- src/netlify/functions/products/index.ts
+++ src/netlify/functions/products/index.ts
@@ -3,10 +3,10 @@ import postgres from "postgres";
 
 const handler: Handler = async function(event, context) {
   
-    const connectionUri = process.env.POLYSCALE_CUSTOMERS_CONNECTION_URI as string;
+    const connectionUri = process.env.POLYSCALE_POSTGRESQLSUPABASE_CONNECTION_URI as string;
     
     if (!connectionUri) {
-        throw new Error("POLYSCALE_CUSTOMERS_CONNECTION_URI no está configurado en las variables de entorno.");
+        throw new Error("process.env.POLYSCALE_POSTGRESQLSUPABASE_CONNECTION_URI no está configurado en las variables de entorno.");
     }
 
 const sql = postgres(connectionUri);
